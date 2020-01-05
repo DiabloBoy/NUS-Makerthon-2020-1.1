c996b1a2fffe6f08daf18658ad0e87f625931ad8
CHAPPALI <e0030842@u.nus.edu>
2020-01-05 16:28:04 +0800

    Social Anxiety Helper Codes
    New Codes (Test)

---
+++ Social_Anxiety_Helper/Social_Anxiety_Helper_1.5/Social_Anxiety_Helper_1.5.ino
@@ -0,0 +1,155 @@
+/***************************************************
+DFPlayer - A Mini MP3 Player For Arduino
+ <https://www.dfrobot.com/product-1121.html>
+ 
+ ***************************************************
+ This example shows the basic function of library for DFPlayer.
+ 
+ Created 2016-12-07
+ By [Angelo qiao](Angelo.qiao@dfrobot.com)
+ 
+ GNU Lesser General Public License.
+ See <http://www.gnu.org/licenses/> for details.
+ All above must be included in any redistribution
+ ****************************************************/
+
+/***********Notice and Trouble shooting***************
+ 1.Connection and Diagram can be found here
+ <https://www.dfrobot.com/wiki/index.php/DFPlayer_Mini_SKU:DFR0299#Connection_Diagram>
+ 2.This code is tested on Arduino Uno, Leonardo, Mega boards.
+ ****************************************************/
+
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
+#include <Wire.h>
+
+// Pins 10 and 11 are used by DF player, so another set of pins are needed for the Bluetooth Module
+SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
+void printDetail(uint8_t type, int value);
+
+int limit = 80;
+int heart_rate;
+int inPin = 2;
+int val = 0;
+
+void setup()
+{
+  mySoftwareSerial.begin(9600);
+  
+  // Baud rate
+  Serial.begin(9600);
+  Wire.begin();
+  
+  Serial.println();
+  Serial.println(F("DFRobot DFPlayer Mini Demo"));
+  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+  
+  if (!myDFPlayer.begin(mySoftwareSerial)) { //Use softwareSerial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while(true){
+    delay(0); // Code to compatible with ESP8266 watch dog.
+    }
+  }
+  Serial.println(F("DFPlayer Mini online."));
+  
+  myDFPlayer.volume(15);  //Set volume value. From 0 to 30
+  
+    Wire.requestFrom(0xA0 >> 1, 1);         // request 1 bytes from slave device
+  
+}
+
+void loop()
+{
+        unsigned char c = Wire.read();   // receive heart rate value (a byte)
+        //Serial.println(c, DEC);
+        heart_rate = int(c);
+        //Serial.println(heart_rate);
+
+      if(heart_rate > limit ) {
+        
+        myDFPlayer.play(1);
+        
+        // Bluetooth Message Alert
+        Serial.print(3);
+        
+        //Pause Playback if push button is pressed
+        
+        for(int counter = 0 ; counter < 500; counter ++){
+            delay(100);            
+            val = digitalRead(inPin);
+            if (val == HIGH){ 
+              myDFPlayer.pause();
+              break;
+            }
+        }
+      }
+   
+
+
+}
+
+void printDetail(uint8_t type, int value){
+  switch (type) {
+    case TimeOut:
+      Serial.println(F("Time Out!"));
+      break;
+    case WrongStack:
+      Serial.println(F("Stack Wrong!"));
+      break;
+    case DFPlayerCardInserted:
+      Serial.println(F("Card Inserted!"));
+      break;
+    case DFPlayerCardRemoved:
+      Serial.println(F("Card Removed!"));
+      break;
+    case DFPlayerCardOnline:
+      Serial.println(F("Card Online!"));
+      break;
+    case DFPlayerUSBInserted:
+      Serial.println("USB Inserted!");
+      break;
+    case DFPlayerUSBRemoved:
+      Serial.println("USB Removed!");
+      break;
+    case DFPlayerPlayFinished:
+      Serial.print(F("Number:"));
+      Serial.print(value);
+      Serial.println(F(" Play Finished!"));
+      break;
+    case DFPlayerError:
+      Serial.print(F("DFPlayerError:"));
+      switch (value) {
+        case Busy:
+          Serial.println(F("Card not found"));
+          break;
+        case Sleeping:
+          Serial.println(F("Sleeping"));
+          break;
+        case SerialWrongStack:
+          Serial.println(F("Get Wrong Stack"));
+          break;
+        case CheckSumNotMatch:
+          Serial.println(F("Check Sum Not Match"));
+          break;
+        case FileIndexOut:
+          Serial.println(F("File Index Out of Bound"));
+          break;
+        case FileMismatch:
+          Serial.println(F("Cannot Find File"));
+          break;
+        case Advertise:
+          Serial.println(F("In Advertise"));
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
+  
+}
